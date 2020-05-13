301d883565fd2a88f0095f0b5b945fa0244b6ba2
Abhishek <59018927+nihalkza@users.noreply.github.com>
2020-05-13 13:41:09 +0530

    shadowgenerator
    initial commit

---
+++ precache-manifest.e487e24b50ac4f83ebd65ac51696b6a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f3e2f8903588d3de6ae105ec873c81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d00772c8b2a5984efe5f",
+    "url": "/static/css/main.ab32de41.chunk.css"
+  },
+  {
+    "revision": "f5d6e3861806e90843c3",
+    "url": "/static/js/2.f9808cf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9808cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00772c8b2a5984efe5f",
+    "url": "/static/js/main.d11121e5.chunk.js"
+  },
+  {
+    "revision": "09a9b56683e072bad95b",
+    "url": "/static/js/runtime-main.f9227652.js"
+  }
+]);
