7f258d5f471aaff023a092f2852e4a6b46c7d77f
Abhishek <abhishek.kumar@ucertify.com>
2020-05-22 18:18:12 +0530

    Updates

---
+++ precache-manifest.f95a061413bfe4287ddc55aa56950399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591e877a4df7c90522b0c0211717c0d0",
+    "url": "/shadow-generator/index.html"
+  },
+  {
+    "revision": "e483a60b9397ef0c55bf",
+    "url": "/shadow-generator/static/css/main.ab32de41.chunk.css"
+  },
+  {
+    "revision": "1512ad3285601a156a25",
+    "url": "/shadow-generator/static/js/2.26bba23b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shadow-generator/static/js/2.26bba23b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e483a60b9397ef0c55bf",
+    "url": "/shadow-generator/static/js/main.da680c68.chunk.js"
+  },
+  {
+    "revision": "eeb2eea75a857d64695e",
+    "url": "/shadow-generator/static/js/runtime-main.265f74aa.js"
+  }
+]);
